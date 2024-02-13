b4a91b694f74c576648cd043d9355c17ca99545b
Sadamori Kojaku <skojaku@iu.edu>
2024-02-12 21:54:49 -0500

    add test

--- tests/test.py
+++ tests/test.py
@@ -1,3 +1,4 @@
+# %%
 import unittest
 import numpy as np
 import sys
@@ -7,11 +8,15 @@ sys.path.append("assignments/")
 from assignment import *
 from scipy import stats
 from sklearn.svm import SVC
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
+from sklearn.metrics import normalized_mutual_info_score
+from sklearn.datasets import make_blobs
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
 
 class TestDimensionalityReduction(unittest.TestCase):
     def setUp(self):
-        from sklearn.datasets import make_blobs
 
         self.X, self.y = make_blobs(
             n_samples=100, centers=3, n_features=3, random_state=42
@@ -49,5 +54,49 @@ class TestDimensionalityReduction(unittest.TestCase):
         self.assertEqual(X_transformed.shape, (100, 2))
 
 
+class TestAdversarialExample(unittest.TestCase):
+    def setUp(self):
+        self.n_samples = 1000
+        self.n_features = 3
+
+    def test_pca_adversarial_example(self):
+
+        dataset = AdversarialExamples()
+
+        min_score = 0.5
+
+        for i in range(10):
+            X, y = dataset.pca_adversarial_data(
+                n_samples=self.n_samples, n_features=self.n_features
+            )
+            pca = PCA(n_components=1)
+            X_transformed = pca.fit_transform(X)
+
+            k = np.max(y + 1)
+            model = KMeans(n_clusters=int(k))
+            model.fit(X)
+            y_pred = model.predict(X)
+
+            model = KMeans(n_clusters=int(k))
+            model.fit(X_transformed)
+            y_pred_transformed = model.predict(X_transformed)
+
+            score = normalized_mutual_info_score(y, y_pred)
+            score_transformed = normalized_mutual_info_score(y, y_pred_transformed)
+
+            if score > 0.99 and score_transformed < min_score:
+                break
+
+        assert (
+            score > 0.99
+        ), f"The generated data does not have well-separated clusters. separability = (original: {score}, pca: {score_transformed})"
+
+        assert (
+            score_transformed < min_score
+        ), f"The clusters should not be separable in the PCA projection. separability = (original: {score}, pca: {score_transformed})"
+
+
 if __name__ == "__main__":
     unittest.main()
+
+# %%
